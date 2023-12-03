71b834556cc37414757605cd5b5492779338b4db
LissieCoutts <lissietn@gmail.com>
2023-12-03 18:41:52 +0000

    Clean up

--- problem_1/part_1.hs
+++ problem_1/part_1.hs
@@ -3,16 +3,13 @@ import Data.Char (isDigit, digitToInt)
 getNumberFromArray :: String -> String -- Returns string of the numbers on that line
 getNumberFromArray textBlob = filter isDigit textBlob
 
-takeFirstAndLast :: String -> String -- Takes the first and last item from a string
-takeFirstAndLast numbers = [head numbers, last numbers]
-
-getFirstAndLast :: String -> Int -- Sums first and last digits
-getFirstAndLast numbers = read $ takeFirstAndLast numbers
+takeFirstAndLast :: String -> Int -- Takes the first and last item from a string
+takeFirstAndLast numbers = read [head numbers, last numbers]
 
 main :: IO ()
 main = do
       input <- readFile "input.txt"
       let linesArray = lines input
-      let totalSum = sum $ map (getFirstAndLast . getNumberFromArray) linesArray
+      let totalSum = sum $ map (takeFirstAndLast . getNumberFromArray) linesArray
       putStrLn $ "Total sum: " ++ show totalSum
       -- mapM_ (putStrLn . show . getFirstAndLast . getNumberFromArray) linesArray
