976d352abd172a469ec9387619f46b7e4c2e3c70
LissieCoutts <lissietn@gmail.com>
2023-12-04 10:19:46 +0000

    Fix part 2

--- problem_1/part_2.hs
+++ problem_1/part_2.hs
@@ -9,15 +9,15 @@ takeFirstAndLast numbers = read [head numbers, last numbers]
 -- The problem here is that it works left to right. If the last number on the line is for example twone, then do we want "2" or "1" ?
 replaceWordsWithDigits :: String -> String
 replaceWordsWithDigits [] = []
-replaceWordsWithDigits ('o' : 'n' : 'e' : rest) = '1' : replaceWordsWithDigits rest
-replaceWordsWithDigits ('t' : 'w' : 'o' : rest) = '2' : replaceWordsWithDigits rest
-replaceWordsWithDigits ('t' : 'h' : 'r' : 'e' : 'e' : rest) = '3' : replaceWordsWithDigits rest
+replaceWordsWithDigits ('o' : 'n' : 'e' : rest) = '1' : replaceWordsWithDigits ('e' : rest) -- check for eight
+replaceWordsWithDigits ('t' : 'w' : 'o' : rest) = '2' : replaceWordsWithDigits ('o' : rest) -- check for one
+replaceWordsWithDigits ('t' : 'h' : 'r' : 'e' : 'e' : rest) = '3' : replaceWordsWithDigits ('e' : rest) -- check for eight
 replaceWordsWithDigits ('f' : 'o' : 'u' : 'r' : rest) = '4' : replaceWordsWithDigits rest
-replaceWordsWithDigits ('f' : 'i' : 'v' : 'e' : rest) = '5' : replaceWordsWithDigits rest
+replaceWordsWithDigits ('f' : 'i' : 'v' : 'e' : rest) = '5' : replaceWordsWithDigits ('e' : rest) -- check for eight
 replaceWordsWithDigits ('s' : 'i' : 'x' : rest) = '6' : replaceWordsWithDigits rest
-replaceWordsWithDigits ('s' : 'e' : 'v' : 'e' : 'n' : rest) = '7' : replaceWordsWithDigits rest
-replaceWordsWithDigits ('e' : 'i' : 'g' : 'h' : 't' : rest) = '8' : replaceWordsWithDigits rest
-replaceWordsWithDigits ('n' : 'i' : 'n' : 'e' : rest) = '9' : replaceWordsWithDigits rest
+replaceWordsWithDigits ('s' : 'e' : 'v' : 'e' : 'n' : rest) = '7' : replaceWordsWithDigits ('n' : rest) -- check for nine
+replaceWordsWithDigits ('e' : 'i' : 'g' : 'h' : 't' : rest) = '8' : replaceWordsWithDigits ('t' : rest) -- check for three
+replaceWordsWithDigits ('n' : 'i' : 'n' : 'e' : rest) = '9' : replaceWordsWithDigits ('e' : rest) -- check for eight
 replaceWordsWithDigits (x : xs) = x : replaceWordsWithDigits xs
 
 part_2 :: [String] -> Int
